7082822846732810d287dba58838a023743e44a2
haldaniko <haldaniko@gmail.com>
2024-08-18 16:21:29 +0200

    fix validators and flake8

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-import json
+import io
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+
 from car.models import Car
 from car.serializers import CarSerializer
 
 
-def serialize_car_object(car: Car) -> str:
+def serialize_car_object(car: Car) -> bytes:
     serializer = CarSerializer(car)
-    return json.dumps(serializer.data)
+    json = JSONRenderer().render(serializer.data)
+    return json
 
 
-def deserialize_car_object(car_json: str) -> Car:
-    data = json.loads(car_json)
+def deserialize_car_object(json: bytes) -> Car:
+    stream = io.BytesIO(json)
+    data = JSONParser().parse(stream)
+    car_id = data.pop("id", None)
     serializer = CarSerializer(data=data)
-    if serializer.is_valid():
-        return serializer.save()
-    else:
-        raise ValueError("Invalid data for deserialization")
+    serializer.is_valid(raise_exception=True)
+    car = serializer.save()
+    if car_id is not None:
+        car.id = car_id
+        car.save()
+    return car
