ff4bb5e740f7bc2357a7161898145110dd2fbf42
haldaniko <haldaniko@gmail.com>
2024-08-18 16:09:11 +0200

    implement 'serialize_car_object' and 'deserialize_car_object' func

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+import json
 from car.models import Car
+from car.serializers import CarSerializer
 
 
-def serialize_car_object(car: Car) -> bytes:
-    pass
+def serialize_car_object(car: Car) -> str:
+    serializer = CarSerializer(car)
+    return json.dumps(serializer.data)
 
 
-def deserialize_car_object(json: bytes) -> Car:
-    pass
+def deserialize_car_object(car_json: str) -> Car:
+    data = json.loads(car_json)
+    serializer = CarSerializer(data=data)
+    if serializer.is_valid():
+        return serializer.save()
+    else:
+        raise ValueError("Invalid data for deserialization")
